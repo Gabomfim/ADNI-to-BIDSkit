f9ea9f04c41fb65e44034f13455e357e1304b92a
Gabomfim <gabomfim99@gmail.com>
2022-08-09 22:10:40 -0300

    adding script

---
+++ adni_to_bidskit.py
@@ -0,0 +1,41 @@
+import argparse
+import pathlib
+import shutil
+import json
+
+def get_BIDS_name(dirname):
+  return dirname.replace('-','').replace('_','').replace('.','')
+
+parser = argparse.ArgumentParser(description='rearranges an ADNI dataset according to BIDS standard.')
+parser.add_argument('-d', '--dataset', required=True,
+                    help='path to the dataset you wish rearrange accordingly to BIDS standard.')
+
+args = parser.parse_args()
+
+dataset_path = pathlib.Path(args.dataset)
+output_path = pathlib.Path('sourcedata')
+
+shutil.rmtree(output_path, ignore_errors=True)
+
+output_path.mkdir(exist_ok=True)
+
+subject_paths = dataset_path.glob('*')
+subject_dirs = [subject_dir for subject_dir in subject_paths if subject_dir.is_dir()]
+
+for index, subject_dir in enumerate(subject_dirs):
+  new_subject_name = get_BIDS_name(subject_dir.name)
+
+  new_subject_path = output_path.joinpath(new_subject_name)
+  new_subject_path.mkdir(parents=True, exist_ok=True)
+
+  session_paths = subject_dir.glob('*/*')
+  session_dirs = [session_dir for session_dir in session_paths if session_dir.is_dir()]
+  for index, session_dir in enumerate(session_dirs):
+    new_session_name = str(index+1).zfill(3)
+
+    new_session_path = new_subject_path.joinpath(new_session_name)
+    new_session_path.mkdir(parents=True, exist_ok=True)
+
+    for index, file_path in enumerate(session_dir.iterdir()):
+      new_file_path = new_session_path.joinpath(get_BIDS_name(file_path.stem)+'.dcm')
+      shutil.copy(file_path, new_file_path)
